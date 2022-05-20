6624abb772633c1d21cdeee6401d28b4a3b6de9a
Argost <cambriayang@qq.com>
2022-05-20 16:18:55 +0800

    Update

--- lib/test/scroll_animation.dart
+++ lib/test/scroll_animation.dart
@@ -10,25 +10,23 @@ class ScrollAnimation extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _ScrollAnimationState();
   }
-
-
 }
 
 class Config extends ChangeNotifier {
-  ScrollPhysics scrollPhysics=NeverScrollableScrollPhysics();
-  double scrollHeight=200;
+  ScrollPhysics scrollPhysics = NeverScrollableScrollPhysics();
+  double scrollHeight = 200;
 }
 
 class _ScrollAnimationState extends State<ScrollAnimation> {
   Config config = Config();
 
-  ScrollController controller=ScrollController();
+  ScrollController controller = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    if(controller.hasListeners==false){
+    if (controller.hasListeners == false) {
       // controller.addListener(() {
       //   print("==[offset: ${controller.offset}");
       //   if(controller.offset<=-50){
@@ -61,10 +59,10 @@ class _ScrollAnimationState extends State<ScrollAnimation> {
             height: config.scrollHeight,
             child: Listener(
               // behavior: HitTestBehavior.opaque,
-              onPointerMove:(PointerMoveEvent event){
+              onPointerMove: (PointerMoveEvent event) {
                 print("onPointerMove==[${event.delta.dy}");
                 print("controller==[${controller.offset}");
-                config.scrollHeight-=event.delta.dy;
+                config.scrollHeight -= event.delta.dy;
 
                 if (config.scrollHeight >= 600) {
                   config.scrollHeight = 600;
@@ -75,14 +73,61 @@ class _ScrollAnimationState extends State<ScrollAnimation> {
                 }
 
                 setState(() {});
-              } ,
-              child: SingleChildScrollView(
+              },
+              child: CustomScrollView(
                 physics: config.scrollPhysics,
                 controller: controller,
-                child: Container(
-                  color: Colors.red,
-                  height: 1400,
-                ),
+                slivers: [
+                  // AppBar，包含一个导航栏.
+                  SliverAppBar(
+                    pinned: true, // 滑动到顶端时会固定住
+                    expandedHeight: 250.0,
+                    flexibleSpace: FlexibleSpaceBar(
+                      title: const Text('Demo'),
+                      background: Image.asset(
+                        'assets/images/sanjiao.png',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(8.0),
+                    sliver: SliverGrid(
+                      //Grid
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2, //Grid按两列显示
+                        mainAxisSpacing: 10.0,
+                        crossAxisSpacing: 10.0,
+                        childAspectRatio: 4.0,
+                      ),
+                      delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                          //创建子widget
+                          return Container(
+                            alignment: Alignment.center,
+                            color: Colors.cyan[100 * (index % 9)],
+                            child: Text('grid item $index'),
+                          );
+                        },
+                        childCount: 20,
+                      ),
+                    ),
+                  ),
+                  SliverFixedExtentList(
+                    itemExtent: 50.0,
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        //创建列表项
+                        return Container(
+                          alignment: Alignment.center,
+                          color: Colors.lightBlue[100 * (index % 9)],
+                          child: Text('list item $index'),
+                        );
+                      },
+                      childCount: 20,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
