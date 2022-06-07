394f624c50c651c95d5b9acb20becbc935bd5f66
cambriayang <yeyang500@hellobike.com>
2022-06-07 20:13:03 +0800

    Update center slice

--- lib/test/center_slice.dart
+++ lib/test/center_slice.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_adjacent_string_concatenation
+
 import 'package:flutter/material.dart';
 
 class CenterSlice extends StatelessWidget {
@@ -13,11 +15,37 @@ class CenterSlice extends StatelessWidget {
         child: Column(
           children: [
             _bubbleBody(),
+            _arrowBody(),
           ],
         ),
       ),
     );
   }
+}
+
+_arrowBody() {
+  return Stack(children: <Widget>[
+    Positioned(
+        child: Image.asset(
+          "assets/images/text_bg.png",
+          fit: BoxFit.fill,
+          centerSlice: Rect.fromLTRB(10.0, 2.0, 12.0, 46.0),
+          // centerSlice: Rect.fromLTWH(10.0, 2.0, 15.0, 40.0),
+        ),
+        left: 0.0,
+        right: 0.0,
+        top: 0.0,
+        bottom: 0.0),
+    Container(
+      width: 200,
+      height: 60,
+      alignment: Alignment.centerLeft,
+      child: Text("#" + "全新为拆封未激活平果",
+          style: TextStyle(fontSize: 12.0, color: Colors.white),
+          textAlign: TextAlign.center),
+      padding: EdgeInsets.only(left: 20),
+    ),
+  ]);
 }
 
 _bubbleBody() {
