855fe9278a03244d899427e078c37f7ded84215f
yeyang500 <yeyang500@hellobike.com>
2024-09-20 17:04:55 +0800

    删除无用代码

--- lib/main.dart
+++ lib/main.dart
@@ -67,13 +67,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    List<String> tests = ["AnimatedWidgetsTest"];
+    // 删除未使用的变量
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
